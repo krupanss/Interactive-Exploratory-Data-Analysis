d40564df0a7845f3db02e5c34dee72f215a2e786
Sai Krupan Seela <krupan@outlook.com>
2017-10-13 16:13:19 +0530

    Version 1.0 files

---
+++ R/ui_1DataSource.R
@@ -0,0 +1,53 @@
+fluidPage(
+  #supress error messages in app
+  # tags$style(    type='text/css',
+  #                ".shiny-output-error{ visibility: hidden;}",
+  #                ".shiny-output-error:before { visibility:hidden; }"
+  # ),
+  
+  ##to reset input values upon data change
+  tags$head(tags$script(
+    HTML(
+      "Shiny.addCustomMessageHandler(
+      'resetValue',function(variableName) {
+      Shiny.onInputChange(variableName, null);});"
+  )
+  )),
+  
+  fluidRow(
+    box(title = "Input your data file", width = 4, status = "primary", solidHeader = TRUE,
+        collapsible = TRUE,
+        radioButtons("fileType", "Select File Type", c(Excel = "excel", `R Data File` = "rda", CSV = "csv"),
+                     inline = TRUE),
+        uiOutput("FileInput"),
+        uiOutput("SelectSheet"),
+        conditionalPanel("input.fileType == 'excel'|| input.fileType == 'csv'",
+                         checkboxInput("header", "Header", TRUE)),
+        conditionalPanel("input.fileType == 'csv'", 
+                         radioButtons("sep", "Separator", c( Comma = ",", Semicolon = ";", Tab = "\t"), ",",
+                                      inline = TRUE)),
+        actionButton("btSubmit", "Submit Data"),
+        htmlOutput("InputValidation")
+        ), 
+    
+    tabBox(title = "Data Processing", width = 8, id = "InputData",
+           tabPanel("Data Structure", 
+                    conditionalPanel("output.ValFlag", htmlOutput("dataInfo")),
+                    conditionalPanel("output.ValFlag", verbatimTextOutput("strData"))),
+           tabPanel("Data Preview",
+                    conditionalPanel("output.ValFlag", DT::dataTableOutput("DataTable"))),
+           tabPanel("Select Column Features",
+                    conditionalPanel("output.ValFlag", withSpinner(uiOutput("SelDimMeas")), 
+                                     actionButton("btExplore", "Explore Data")))
+           )
+    )
+  # fluidRow(
+  #   box(title = "Data Preview", width = 8, status = "info", solidHeader = TRUE,
+  #       collapsible = TRUE,"Sample"
+  #       ),
+  #   
+  #   box(title = "Select Column Features", width = 4, status = "primary", solidHeader = TRUE,
+  #       collapsible = TRUE, "Sample2"
+  #       )
+  #   )
+)
